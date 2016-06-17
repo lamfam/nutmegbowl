8e324133481f1f7ab9078cafd0df8a371b10351b
Jonathan Lam <jlam55555@gmail.com>
2016-06-17 19:23:21 -0400

    Changed footer calculations

--- res/script.js
+++ res/script.js
@@ -3,16 +3,20 @@ $(function() {
   // set element variables
   var doc = $(document);
   var win = $(window);
+  var foot = $("footer");
   
   // accessory functions
   var getPxValue = (input) => parseInt(input.substring(0, input.length-2));
 
   // reposition the footer
   var docWidth, docHeight;
-  var reposition = () => $("footer").css({ top: docHeight-$("footer").height()-getPxValue($("footer").css("padding-top"))*2 });
+  var reposition = () => foot.css({ top: docHeight-foot.height()-getPxValue(foot.css("padding-top"))*2 });
   win.resize(() => {
+    var footer = foot;
+    foot.remove();
     docWidth = doc.width() > win.width() ? doc.width() : win.width();
     docHeight = doc.height() > win.height() ? doc.height() : win.height();
+    $("body").append(footer);
     reposition();
   }).resize();
 
