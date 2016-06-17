97ba5459e887c9b822fea3a153d22cb2595d85c3
Jonathan Lam <jlam55555@gmail.com>
2016-06-17 18:19:09 -0400

    Added some footer styling

--- res/script.js
+++ res/script.js
@@ -1,7 +1,16 @@
 $(function() {
+
+  // set element variables
+  var doc = $(document);
+  var win = $(window);
+
+  // document/window dimensions (whichever is larger)
+  var docWidth = doc.width() > win.width() ? doc.width() : win.width();
+  var docHeight = doc.height() > win.height() ? doc.height() : win.height();
   
+  // get footer, header, and styles
   $.get("header.html", html => $("header").html(html));
-  $.get("footer.html", html => $("footer").html(html));
+  $.get("footer.html", html => $("footer").html(html).css({ top: docHeight-$(this).height() }));
   $.get("res/style.css", css => $("head").append("<style>" + css + "</style>"));
 
 });
