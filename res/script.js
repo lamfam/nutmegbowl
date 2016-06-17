d193551d454cfbf9ffcc6f34b5c29683a7f14652
Jonathan Lam <jlam55555@gmail.com>
2016-06-17 19:05:28 -0400

    Edited repositioning script

--- res/script.js
+++ res/script.js
@@ -8,9 +8,17 @@ $(function() {
   var docWidth = doc.width() > win.width() ? doc.width() : win.width();
   var docHeight = doc.height() > win.height() ? doc.height() : win.height();
   
+  // reposition the footer
+  var reposition = () => $("footer").css({ top: docHeight-$("footer").height()+$("footer").css("padding-top")*2 });
+  win.resize(reposition);
+
   // get footer, header, and styles
   $.get("res/style.css", css => $("head").append("<style>" + css + "</style>"));
   $.get("header.html", html => $("header").html(html));
-  $.get("footer.html", html => $("footer").html(html).css({ top: docHeight-$("footer").height() }));
+  $.get("footer.html", html => {
+    $("footer").html(html)
+    reposition();
+  });
+
 
 });
