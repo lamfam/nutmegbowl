de1a707de2b02739eaecb3ae293223df00324584
Jonathan Lam <jlam55555@gmail.com>
2016-06-17 19:09:03 -0400

    Fixed bottom positioning script

--- res/script.js
+++ res/script.js
@@ -8,15 +8,18 @@ $(function() {
   var docWidth = doc.width() > win.width() ? doc.width() : win.width();
   var docHeight = doc.height() > win.height() ? doc.height() : win.height();
   
+  // accessory functions
+  var getPxValue = (input) => parseInt(input.substring(input.length-3, input.length));
+
   // reposition the footer
-  var reposition = () => $("footer").css({ top: docHeight-$("footer").height()+$("footer").css("padding-top")*2 });
+  var reposition = () => $("footer").css({ top: docHeight-$("footer").height()+getPxValue($("footer").css("padding-top"))*2 });
   win.resize(reposition);
 
   // get footer, header, and styles
   $.get("res/style.css", css => $("head").append("<style>" + css + "</style>"));
   $.get("header.html", html => $("header").html(html));
   $.get("footer.html", html => {
-    $("footer").html(html)
+    $("footer").html(html);
     reposition();
   });
 
