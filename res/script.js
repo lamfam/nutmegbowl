4679fb442a047a4cf6cb94822d5d3f3765997412
Jonathan Lam <jlam55555@gmail.com>
2016-06-18 14:49:42 -0400

    Added simple sidebar

--- res/script.js
+++ res/script.js
@@ -6,27 +6,40 @@ $(function() {
   var foot = $("footer");
   
   // accessory functions
-  var getPxValue = (input) => parseInt(input.substring(0, input.length-2));
+  var getPxValue = function(input) {
+    return parseInt(input.substring(0, input.length-2));
+  };
 
   // reposition the footer
   var docWidth, docHeight;
-  var reposition = () => foot.css({ top: docHeight-foot.height()-getPxValue(foot.css("padding-top"))*2 });
-  win.resize(() => {
+  var reposition = function() {
+    foot.css({ top: docHeight-foot.height()-getPxValue(foot.css("padding-top"))*2 });
+  };
+  win.resize(function() {
     var footer = foot;
     foot.remove();
     docWidth = doc.width() > win.width() ? doc.width() : win.width();
     docHeight = doc.height() > win.height() ? doc.height() : win.height();
     $("body").append(footer);
     reposition();
+    $("div#menu").css({ height: win.height() });
   }).resize();
 
   // get footer, header, and styles
   $("head").append("<link rel='stylesheet' type='text/css' href='res/fa.css' /><link rel='stylesheet' type='text/css' href='res/style.css' />");
-  $.get("header.html", html => $("header").html(html));
-  $.get("footer.html", html => {
+  $.get("header.html", function(html) {
+    $("header").html(html);
+    // toggle menu
+    $(".menuButton").click(function() { $("div#menu").toggleClass("hidden") });
+    doc.on("click", "*", function(event) {
+      event.stopPropagation();
+      if(!$(this).is("div#head, div#head *, .menuButton") && !$("div#menu").hasClass("hidden"))
+        $("div#menu").addClass("hidden");
+    });
+  });
+  $.get("footer.html", function(html) {
     $("footer").html(html);
     reposition();
-    
     // popups to share
     $(document).on("click", ".shareButtonLink", function() {
       window.open($(this).attr("href"), "", "menubar=no,toolbar=no,resizable=yes,scrollbars=yes,height=500,width=500");
@@ -35,7 +48,6 @@ $(function() {
     $("#facebookShareButton").attr({ href: "https://www.facebook.com/sharer/sharer.php?u=" + encodeURIComponent(window.location.href) });
     $("#twitterShareButton").attr({ href: "https://twitter.com/intent/tweet?text=" + encodeURIComponent("Come visit at Nutmeg Bowl for the best bowling in Fairfield County! " + window.location.href) });
     $("#googlePlusShareButton").attr({ href: "https://plus.google.com/share?url=" + encodeURIComponent(window.location.href) });
-
   });
- 
+
 });
